dc82985821b3f56775ea3615d059a8fe5fd69acd
Seifert, Jan <jan.seifert@firma.seznam.cz>
2016-07-08 07:55:39 +0200

    setup.py + setup.cfg

--- setup.py
+++ setup.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+import sys
 
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+
+    user_options = [
+        ('pytest-args=', 'a', "Arguments to pass to py.test"),
+    ]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 setup(
     name="tornado-fastrpc",
-    version='0.0.0',
-    author='Seznam.cz, a.s.',
+    version='0.0.1',
+    author='Jan Seifert (Seznam.cz, a.s.)',
     author_email="jan.seifert@firma.seznam.cz",
     description=(
-        "Asynchronous FastPRC client and server (handler) for Python's Tornado"
+        "Asynchronous FastPRC client for Python's Tornado"
     ),
     license="commercial",
     url='ssh://git@gitlab.kancelar.seznam.cz:doporucovani/common.git',
@@ -18,13 +36,33 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    platforms=['any'],
     packages=find_packages('tornado_fastrpc'),
     zip_safe=True,
     install_requires=[
         'setuptools>=0.6b1',
-        'tornado>=3.0',
-        'fastrpc',
+        'tornado>=3.2',
         'pycurl',
     ],
+    extras_require={
+        'FastRPC':  [
+            'fastrpc',
+        ],
+    },
+    tests_require=[
+        'pytest-cov',
+        'pytest',
+        'mock',
+    ],
+    test_suite='tests',
+    cmdclass={
+        'test': PyTest,
+    },
 )
